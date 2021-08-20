12fa37c2bf14ceb27426c2315055360e031312f8
hidwoc <heidischoi@gmail.com>
2021-08-20 17:32:20 -0400

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :projects
+  resources :skills
+    
+  resources :admins, only: [:show, :update]
+  post "/admins/login", to: "admins#login"
+  get "/admins/verify", to: "admins#verify"
 end
