0c6bde7472302f95548ed98a85efaf784747cc75
hidwoc <heidischoi@gmail.com>
2021-08-23 17:31:47 -0400

    Change svg fill

--- client/src/components/ProjectDetails/ProjectDetails.jsx
+++ client/src/components/ProjectDetails/ProjectDetails.jsx
@@ -1,24 +1,26 @@
 import { Link } from "react-router-dom";
+import "./ProjectDetails.css";
 
 const ProjectDetails = ({ project }) => {
   const { id, img, name, techstack, repo, site, description } = project;
 
   return (
-    <div>
-      <div>{img}</div>
-      <div>
-        {name}
-        {techstack}
-        <div>
-          <Link to={{ pathname: { repo } }} target="_blank">
+    <div className="project-details">
+      <div className="project-img">{img}</div>
+      <div className="project-info">
+        <p className="project-name">{name}</p>
+        <p className="project-techstack">{techstack}</p>
+        <div className="project-links">
+          <Link to={{ pathname: repo }} target="_blank">
             <img
               id="github"
               src={`${process.env.PUBLIC_URL}/assets/graphics/github.svg`}
               alt="github logo"
               width="20"
+              style={{fill: "white"}}
             />
           </Link>
-          <Link to={{ pathname: { site } }} target="_blank">
+          <Link to={{ pathname: site }} target="_blank">
             <img
               id="linkedin"
               src={`${process.env.PUBLIC_URL}/assets/graphics/external-link.svg`}
@@ -27,7 +29,7 @@ const ProjectDetails = ({ project }) => {
             />
           </Link>
         </div>
-        {description}
+        <p className="project-description">{description}</p>
       </div>
     </div>
   );
