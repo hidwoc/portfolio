d94bdf9474f7add3749beeb75504a457db6346b6
hidwoc <heidischoi@gmail.com>
2021-08-23 22:03:27 -0400

    Start adding Skill css

--- client/src/components/Skills/Skills.jsx
+++ client/src/components/Skills/Skills.jsx
@@ -4,21 +4,22 @@ import "./Skills.css"
 const Skills = ({ skills }) => {
   return (
     <div id="skills-div">
-      <div className="section-header">Skills</div>
+      <h2 className="section-header">Skills</h2>
       <div className="skills fullstack" id="frontend">
+        <h4>Frontend</h4>
         {skills
           // .filter((skill) => {
           //   skill.category === "Frontend";
           // })
           .map((skill) => (
-            <div key={skill.id}>
-              <img src={skill.icon} width="75"/>
+            <div key={skill.id} className="skill-icon">
+              <img src={skill.icon} width="70"/>
               <p>{skill.name}</p>
             </div>
           ))}
       </div>
-      <div className="skills fullstack" id="backend"></div>
-      <div className="skills" id="other"></div>
+      <div className="skills fullstack" id="backend"><h4>Backend</h4></div>
+      <div className="skills" id="other"><h4>Other</h4></div>
     </div>
   );
 };
